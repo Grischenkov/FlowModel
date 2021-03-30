8d2e58ba9f0084f310b589be833308896b96626e
Nikita <nikita@grishchenkov.ru>
2021-03-30 10:35:47 +0300

    feat(researcherview.cs): add tool strip
    FM-10

--- FlowModel/ResearcherView.Designer.cs
+++ FlowModel/ResearcherView.Designer.cs
@@ -28,22 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.open_ToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.save_ToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.export_ToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.exit_ToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.setting_ToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.help_ToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.about_ToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1904, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.open_ToolStrip,
+            this.save_ToolStrip,
+            this.export_ToolStrip,
+            this.toolStripMenuItem2,
+            this.exit_ToolStrip});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.setting_ToolStrip,
+            this.helpToolStripMenuItem1,
+            this.help_ToolStrip,
+            this.about_ToolStrip});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // open_ToolStrip
+            // 
+            this.open_ToolStrip.Name = "open_ToolStrip";
+            this.open_ToolStrip.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.open_ToolStrip.Size = new System.Drawing.Size(180, 22);
+            this.open_ToolStrip.Text = "Open";
+            // 
+            // save_ToolStrip
+            // 
+            this.save_ToolStrip.Name = "save_ToolStrip";
+            this.save_ToolStrip.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.save_ToolStrip.Size = new System.Drawing.Size(180, 22);
+            this.save_ToolStrip.Text = "Save";
+            // 
+            // export_ToolStrip
+            // 
+            this.export_ToolStrip.Name = "export_ToolStrip";
+            this.export_ToolStrip.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
+            this.export_ToolStrip.Size = new System.Drawing.Size(180, 22);
+            this.export_ToolStrip.Text = "Export";
+            // 
+            // exit_ToolStrip
+            // 
+            this.exit_ToolStrip.Name = "exit_ToolStrip";
+            this.exit_ToolStrip.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.exit_ToolStrip.Size = new System.Drawing.Size(180, 22);
+            this.exit_ToolStrip.Text = "Exit";
+            // 
+            // setting_ToolStrip
+            // 
+            this.setting_ToolStrip.Name = "setting_ToolStrip";
+            this.setting_ToolStrip.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.S)));
+            this.setting_ToolStrip.Size = new System.Drawing.Size(188, 22);
+            this.setting_ToolStrip.Text = "Settings";
+            // 
+            // help_ToolStrip
+            // 
+            this.help_ToolStrip.Name = "help_ToolStrip";
+            this.help_ToolStrip.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.H)));
+            this.help_ToolStrip.Size = new System.Drawing.Size(188, 22);
+            this.help_ToolStrip.Text = "Help";
+            // 
+            // about_ToolStrip
+            // 
+            this.about_ToolStrip.Name = "about_ToolStrip";
+            this.about_ToolStrip.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
+            this.about_ToolStrip.Size = new System.Drawing.Size(188, 22);
+            this.about_ToolStrip.Text = "About";
+            // 
+            // helpToolStripMenuItem1
+            // 
+            this.helpToolStripMenuItem1.Name = "helpToolStripMenuItem1";
+            this.helpToolStripMenuItem1.Size = new System.Drawing.Size(185, 6);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(177, 6);
             // 
             // ResearcherView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.MinimumSize = new System.Drawing.Size(950, 650);
             this.Name = "ResearcherView";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Researcher";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem open_ToolStrip;
+        private System.Windows.Forms.ToolStripMenuItem save_ToolStrip;
+        private System.Windows.Forms.ToolStripMenuItem export_ToolStrip;
+        private System.Windows.Forms.ToolStripMenuItem exit_ToolStrip;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setting_ToolStrip;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripSeparator helpToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem help_ToolStrip;
+        private System.Windows.Forms.ToolStripMenuItem about_ToolStrip;
     }
 }
 
