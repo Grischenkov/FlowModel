473e6cd3d972aa7de092b9244aa5f4c6f840163e
Nikita <nikita@grishchenkov.ru>
2021-03-31 13:51:19 +0300

    feat(iresearcherview.cs): add interface with all needed properties
    FM-14

---
+++ FlowModel.Presenter/Views/ResearcherView/IResearcherView.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace FlowModel.Presenter.Views.ResearcherView
+{
+    public interface IResearcherView : IFileMenuView, IHelpMenuView
+    {
+        string MaterialItem { get; set; }
+        string MaterialSelectedItem { get; set; }
+        
+        string Width { get; set; }
+        string Depth { get; set; }
+        string Length { get; set; }
+        
+        string Density { get; set; }
+        string HeatCapacity { get; set; }
+        string MeltingTemperature { get; set; }
+        
+        string LidSpeed { get; set; }
+        string LidTemperature { get; set; }
+        
+        string Step { get; set; }
+        
+        string FlowIndex { get; set; }
+        string ViscosityIndex { get; set; }
+        string ConsistencyIndex { get; set; }
+        string HeatTransferIndex { get; set; }
+        string ReferenceTemperature { get; set; }
+        
+        string Viscosity { set; }
+        string Temperature { set; }
+        string Performance { set; }
+        
+        string Time { set; }
+        string Memory { set; }
+        
+        event Action Calculate;
+
+        event Action ShowValueTable;
+        event Action ShowTemperatureChart;
+        event Action ShowViscosityChart;
+    }
+}
