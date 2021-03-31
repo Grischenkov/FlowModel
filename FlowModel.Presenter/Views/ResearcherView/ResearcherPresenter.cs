c3325887fcd3dd3c8dce5bd9ccce50fc26cf58a3
Nikita <nikita@grishchenkov.ru>
2021-03-31 14:39:37 +0300

    feat(researcherpresenter.cs): add presenter class
    Add presenter class with event subscription but without functionality implementation and model connection.
    
    FM-13

---
+++ FlowModel.Presenter/Views/ResearcherView/ResearcherPresenter.cs
@@ -0,0 +1,69 @@
+﻿using FlowModel.Presenter.ParentInterfaces;
+
+namespace FlowModel.Presenter.Views.ResearcherView
+{
+    public class ResearcherPresenter : BasePresenter<IResearcherView>
+    {
+        public ResearcherPresenter(IApplicationController controller, IResearcherView view) : base(controller, view)
+        {
+            View.Calculate += Calculate;
+
+            View.ShowValueTable += ShowValueTable;
+            View.ShowTemperatureChart += ShowTemperatureChart;
+            View.ShowViscosityChart += ShowViscosityChart;
+            
+            View.Open += Open;
+            View.Save += Save;
+            View.Export += Export;
+            View.Exit += Exit;
+
+            View.Settings += Settings;
+            View.Help += Help;
+            View.About += About;
+        }
+
+        private void Calculate()
+        {
+            //TODO Implement calculation algorithm
+        }
+
+        private void ShowValueTable()
+        {
+            //Controller.Run<ValueTablePresenter>();
+        }
+        private void ShowTemperatureChart()
+        {
+            //Controller.Run<TemperatureChartPresenter>();
+        }
+        private void ShowViscosityChart()
+        {
+            //Controller.Run<ViscosityChartPresenter>();
+        }
+        
+        private void Open()
+        {
+            //TODO Implement loading functionality
+        }
+        private void Save()
+        {
+            //TODO Implement saving functionality            
+        }
+        private void Export()
+        {
+            //TODO Implement export functionality
+        }
+
+        private void Settings()
+        {
+            //Controller.Run<SettingsPresenter>();
+        }
+        private void Help()
+        {
+            //Controller.Run<HelpPresenter>();
+        }
+        private void About()
+        {
+            //Controller.Run<AboutPresenter>();
+        }
+    }
+}
