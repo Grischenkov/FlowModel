b38cb2a4dd0fd832057064c0234f5124a4fe6db5
Nikita Grishchenkov <nikita@grishchenkov.ru>
2021-04-02 22:04:20 +0300

    feat: add additional model classes
    Add next additional model classes:
    - Channel
    - Cap
    
    FM-19

---
+++ FlowModel.Model/Cap.cs
@@ -0,0 +1,9 @@
+﻿namespace FlowModel.Model
+{
+    public class Cap
+    {
+        public double Speed { get; set; }
+        
+        public double Temperature { get; set; }
+    }
+}
---
+++ FlowModel.Model/Channel.cs
@@ -0,0 +1,13 @@
+﻿namespace FlowModel.Model
+{
+    public class Channel
+    {
+        public double Width { get; set; }
+        
+        public double Depth { get; set; }
+        
+        public double Length { get; set; }
+        
+        public double CalculationStep { get; set; }
+    }
+}
