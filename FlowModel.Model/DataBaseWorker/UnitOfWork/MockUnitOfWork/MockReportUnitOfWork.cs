f02ee02d09048a513ccb8c69c6e2f1b0eec5569a
Nikita <nikita@grishchenkov.ru>
2021-04-09 10:48:31 +0300

    feat: add database mock classes
    FM-22

---
+++ FlowModel.Model/DataBaseWorker/UnitOfWork/MockUnitOfWork/MockReportUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace FlowModel.Model
+{
+    public class MockReportUnitOfWork : UnitOfWork
+    {
+        private MockRepository<Report> _reports;
+
+        public IRepository<Report> Reports => _reports ?? (_reports = new MockRepository<Report>());
+    }
+}
---
+++ FlowModel.Model/DataBaseWorker/UnitOfWork/MockUnitOfWork/MockUserUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace FlowModel.Model
+{
+    public class MockUserUnitOfWork : UnitOfWork
+    {
+        private MockRepository<User> _users;
+
+        public IRepository<User> Users => _users ?? (_users = new MockRepository<User>());
+    }
+}
