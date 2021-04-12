33ccf40d2b8ffacca827a9fbee3d5a3a6b68fbdb
Nikita <nikita@grishchenkov.ru>
2021-04-12 21:08:27 +0300

    feat: add chart view interface
    FM-33

---
+++ FlowModel.Presenter/Views/ChartView/IChartView.cs
@@ -0,0 +1,13 @@
+﻿using FlowModel.Presenter.ParentInterfaces;
+
+namespace FlowModel.Presenter.Views.ChartView
+{
+    public interface IChartView : IView
+    {
+        string ChartName { get; set; }
+        
+        string XAxisName { get; set; }
+        
+        string YAxisName { get; set; }
+    }
+}
