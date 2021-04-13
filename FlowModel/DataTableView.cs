2c1d5d51c1cdd42e50f14fd39b4f245d31049b1d
Nikita Grishchenkov <nikita@grishchenkov.ru>
2021-04-13 23:14:11 +0300

    feat: add data table view
    FM-34

---
+++ FlowModel/DataTableView.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Forms;
+
+namespace FlowModel
+{
+    public partial class DataTableView : Form, IDataTableView
+    {
+        public DataTableView()
+        {
+            InitializeComponent();
+        }
+        
+        public void DrawTable(double[,] table)
+        {
+            for (int i = 0, j = 0; i < table.Length; i++)
+            {
+                dataGridView.RowCount = j + 1;
+                dataGridView.Rows[j].Cells[0].Value = table[i,0].ToString();
+                dataGridView.Rows[j].Cells[1].Value = table[i,1].ToString();
+                dataGridView.Rows[j].Cells[1].Value = table[i,2].ToString();
+                j++;
+            }
+        }
+        
+        public new void Show()
+        {
+            ShowDialog();
+        }
+    }
+    
+}
